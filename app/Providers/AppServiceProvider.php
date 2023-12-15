776e3d064085527c8cbcf3ca2fa6731a1ee00838
Afolayan Toluwalope <25456358+timisi@users.noreply.github.com>
2023-12-15 16:09:34 +0100

    Updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,28 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Set the default string length for migrations
+        Schema::defaultStringLength(191);
     }
 }
